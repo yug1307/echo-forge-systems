5e946fd26731fb0d97dfdd18378a40ff5b7b2408
yug1307 <161486463+yug1307@users.noreply.github.com>
2026-04-22 16:15:22 +0530

    Adjust layout class for SaaS section

--- src/services/SaaS.jsx
+++ src/services/SaaS.jsx
@@ -186,7 +186,7 @@ const SaaS = () => {
       <section className="bg-zinc-900 py-12">
         <div 
           style={{background:"linear-gradient(to top, #9333ea, #2563eb)"}} 
-          className="flex flex-col items-center rounded-xl py-7 px-5 md:mx-20 text-center space-y-5">
+          className="flex flex-col items-center rounded-xl py-7 px-5 mx-5 sm:mx-10 text-center space-y-5">
             
           <p className="text-3xl font-bold text-white">Ready to Build Your SaaS Product?</p>
 
